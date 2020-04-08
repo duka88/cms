e785b968243f5c23bcb27710617dc80ce7ba792e
duka88 <djankov88@gmail.com>
2020-04-08 11:22:12 +0200

    'mailSend'

--- resources/views/email/P8Mail.blade.php
+++ resources/views/email/P8Mail.blade.php
@@ -4,9 +4,19 @@
     <title>Passage8.com</title>
 </head>
 <body>
+
     <h1>{{ $details['title'] }}</h1>
-    <p>{{ $details['body'] }}</p>
-   
+    @foreach($details['body'] as $key => $value )
+
+
+      <p><b>{{$key}}</b>: 
+      @if(is_array($value))
+         {{ implode( ", ", $value ) }}
+      @else
+         {{ $value }}
+      @endif
+  </p>   
+    @endforeach
    
 </body>
 </html>
